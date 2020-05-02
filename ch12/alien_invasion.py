d18f9247dad971ed7986c821c8d1e3bed1f6fac0
lixinaha <lilnelse@163.com>
2020-05-02 22:56:39 +0800

    fix error and warnings

--- ch12/alien_invasion.py
+++ ch12/alien_invasion.py
@@ -1,19 +1,19 @@
-import sys
+# import sys
 import pygame
 
 from settings import Settings
 from ship import Ship
-import game_functions as gf 
+import game_functions as gf
 
 def run_game():
     pygame.init()
     ai_setting = Settings()
-    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))    
+    screen = pygame.display.set_mode((ai_setting.screen_width, ai_setting.screen_height))
     pygame.display.set_caption("Alien Invation")
 
     ship = Ship(ai_setting, screen)
 
-    while(True):
+    while True:
         gf.check_events(ship)
         ship.update()
         gf.update_screen(ai_setting, screen, ship)
